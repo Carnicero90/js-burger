f07c2786ff90bde958125832f593907cca7fc09b
Carnicero90 <filippomontani82@gmail.com>
2021-03-30 14:58:09 +0200

    aggiunta sorta di animazione

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const basePrice = 50;
+const basePrice = 15;
 
 document.getElementById("output").innerHTML = basePrice.toLocaleString("it-IT", {
     minimumFractionDigits: 2,
@@ -22,8 +22,15 @@ calculatePrice.addEventListener("click", function() {
     if (fakeCoupons.includes(discountCode)) {
         finalPrice *= 0.9; //facciamo uno sconto del 10% per tutti i codici, per ora
     }
-    document.getElementById("output").innerHTML = finalPrice.toLocaleString("it-IT", {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-    });
+    document.getElementById('logging-wrapper').classList.remove('nada');
+    document.getElementById('burger-wrapper').classList.add('opaq');
+    setTimeout(function() {
+        document.getElementById('logging-wrapper').classList.add('nada');
+        document.getElementById('burger-wrapper').classList.remove('opaq');
+        document.getElementById("output").innerHTML = finalPrice.toLocaleString("it-IT", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        });
+    }, 3000);
+
 })
