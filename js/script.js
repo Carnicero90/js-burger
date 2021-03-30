3fca3a0f86e00615fe349cb6eed09f555799a59a
Carnicero90 <filippomontani82@gmail.com>
2021-03-30 15:16:49 +0200

    reso obbligatorio inserimento nome burger

--- js/script.js
+++ js/script.js
@@ -9,28 +9,33 @@ document.getElementById("discount-code").value = "";
 var calculatePrice = document.getElementById('calc-price');
 
 calculatePrice.addEventListener("click", function() {
-    var fakeCoupons = ["xananx12", "perfavore", "voglio_sconto", "z"];
-    var finalPrice = basePrice;
-    var ingredients = document.getElementsByClassName("ingredient-check");
+    var burgerName = document.getElementById("burger-name").value;
+    if (!burgerName) {
+        alert("Inserire nome Burger!!!");
+    } else {
+        var fakeCoupons = ["xananx12", "perfavore", "voglio_sconto", "z"];
+        var finalPrice = basePrice;
+        var ingredients = document.getElementsByClassName("ingredient-check");
 
-    for (ingredient of ingredients) {
-        if (ingredient.checked) {
-            finalPrice += parseInt(ingredient.value);
+        for (ingredient of ingredients) {
+            if (ingredient.checked) {
+                finalPrice += parseInt(ingredient.value);
+            }
         }
+        var discountCode = document.getElementById('discount-code').value;
+        if (fakeCoupons.includes(discountCode)) {
+            finalPrice *= 0.9; //facciamo uno sconto del 10% per tutti i codici, per ora
+        }
+        document.getElementById('logging-wrapper').classList.remove('nada');
+        document.getElementById('burger-wrapper').classList.add('opaq');
+        setTimeout(function() {
+            document.getElementById('logging-wrapper').classList.add('nada');
+            document.getElementById('burger-wrapper').classList.remove('opaq');
+            document.getElementById("output").innerHTML = finalPrice.toLocaleString("it-IT", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            });
+        }, 3000);
     }
-    var discountCode = document.getElementById('discount-code').value;
-    if (fakeCoupons.includes(discountCode)) {
-        finalPrice *= 0.9; //facciamo uno sconto del 10% per tutti i codici, per ora
-    }
-    document.getElementById('logging-wrapper').classList.remove('nada');
-    document.getElementById('burger-wrapper').classList.add('opaq');
-    setTimeout(function() {
-        document.getElementById('logging-wrapper').classList.add('nada');
-        document.getElementById('burger-wrapper').classList.remove('opaq');
-        document.getElementById("output").innerHTML = finalPrice.toLocaleString("it-IT", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        });
-    }, 3000);
 
 })
